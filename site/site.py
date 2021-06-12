087dcb9fffa4252c2793ca4af7e3b7ec3b1526dd
Xikka56 <70409596+Xikka56@users.noreply.github.com>
2021-06-12 11:26:36 +0300

    Бэк

--- site/site.py
+++ site/site.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, session, Response, make_response, redirect, jsonify
+from flask import Flask, render_template, request, session, Response, make_response, redirect, jsonify, abort
 from flask_cors import CORS
 import datetime
 import uuid
 import hashlib
-import sqlite3
 import pymysql
 import time
 import string
@@ -29,8 +28,8 @@ class DB():
     def GET(self):
         """Получает данные с Базы данных
         """
-        connection = pymysql.connect(host="37.140.192.90", user='u1102095_ting',
-                                     password='S7x6U9j2', database='u1102095_tg_bot', charset="utf8mb4")
+        connection = pymysql.connect(host="31.31.196.245", user='u0981115',
+                                     password='qwert5656', database='u0981115_itsunrise', charset="utf8mb4")
         cursor = connection.cursor()
         cursor.execute(self)
         OTV = cursor.fetchall()
@@ -39,8 +38,8 @@ class DB():
     def POST(self):
         """Отправляет данные в Базу данных
         """
-        connection = pymysql.connect(host="37.140.192.90", user='u1102095_ting',
-                                     password='S7x6U9j2', database='u1102095_tg_bot', charset="utf8")
+        connection = pymysql.connect(host="31.31.196.245", user='u0981115',
+                                     password='qwert5656', database='u0981115_itsunrise', charset="utf8")
         cursor = connection.cursor()
         cursor.execute(self)
         connection.commit()
@@ -53,18 +52,53 @@ def index():
     return render_template("index.html")
 
 
-@application.route('/num', methods=["GET", "POST"])
-def num_print():
+@application.route('/api/0.1/get_prof', methods=["POST"])
+def get_prof():
     # response_data = request.data.decode()
+    # print("data", response_data)
     # response_data = json.loads(response_data)
-    # print(response_data["null"])
-    response_data = {"qwe": "qwe"}
-    response_data["status"] = "none"
-    # json.loads(per)
+    prof_id = DB.GET(
+        f"""SELECT * FROM `description_profession` ORDER BY price_junior DESC""")
+    req_data = []
+    for i in prof_id:
+        print(i)
+        g = {
+            "id": i[0],
+            "title": i[1],
+            "description": i[2],
+            "time_learning": i[3],
+            "price_junior": i[4]}
+        req_data.append(g)
+    mass_elem = {"elements": req_data}
+    return mass_elem
 
-    return response_data
-    # name+" " + str(num)
+
+@application.route('/api/0.1/get_circle_data', methods=["POST"])
+def get_circle_data():
+    response_data = request.data.decode()
+    print("data", response_data, len(response_data))
+    response_data = json.loads(response_data)
+    if not "id" in response_data:
+        abort(400)
+    prof_micro_info = DB.GET(
+        f"""SELECT * FROM `description_circle` WHERE id = {response_data["id"]}""")
+    req_data = []
+    i = prof_micro_info[0]
+    mass_elem = {
+        "id": i[0],
+        "name": i[1],
+        "description": i[2],
+        "learning_time": i[3],
+        "type_size": i[4],
+        "type_profession": i[5]}
+    return mass_elem, 200
+
+
+# "95.172.70.131"
+@application.errorhandler(400)
+def error_400(e):
+    return "Bad request", 400
 
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run(debug=True, host="0.0.0.0", port=4567)
